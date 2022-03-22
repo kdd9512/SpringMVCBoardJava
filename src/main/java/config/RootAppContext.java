c78e035fc1c3d5ba089d70a03d0906b2c3aa3518
kdd9512 <hyunwoo8805@gmail.com>
2022-03-22 23:07:04 +0900

    20220322 11th commit
    회원 로그인 처리 구현중.

--- src/main/java/config/RootAppContext.java
+++ src/main/java/config/RootAppContext.java
@@ -1,9 +1,21 @@
 package config;
 
+import beans.MemberInfoBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.SessionScope;
 
 // 프로젝트 작업시 사용할 bean을 정의하는 클래스
 @Configuration
 public class RootAppContext {
+
+    // 로그인 한 사용자의 정보를 담을 MemberInfoBean 에 이름을 붙히고 sessionScope 로 정의한다.
+    // MemberInfoBean 은 회원등록, 수정 및 삭제 등에도 사용되므로 반드시 이름을 붙혀서 구분해서 사용해야 한다.
+    @Bean("loginMemberBean")
+    @SessionScope
+    public MemberInfoBean loginMemberBean() {
+        return new MemberInfoBean();
+    }
+
 
 }
--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,14 +1,23 @@
 package controller;
 
+import beans.MemberInfoBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
+
+
 @Controller
 public class HomeController {
-	
+
+	// loginMemberBean 이 session 으로 들어오고 있는지 확인하기 위한 코드.
+//	@Resource(name = "loginMemberBean")
+//	private MemberInfoBean loginMemberBean;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
+//		System.out.println(loginMemberBean);
 		return "redirect:/main";
 	}
 }
